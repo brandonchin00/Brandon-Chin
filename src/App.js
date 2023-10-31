4c4efee420f3da4c2781100d78929e4626013f8f
Brandon Chin <brandonzchin@gmail.com>
2023-10-31 17:19:03 -0400

    Fix (After NavBar)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Navbar } from "./components/NavBar";
+import About from "./components/about.js";
 export default function App() {
   return (
     <div className="App">
       <Navbar />
+      <About />
     </div>
   );
 }
