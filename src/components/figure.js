a8dd3ffc20ca4057501d3fbd19c90e8f2bfa242c
Brandon Chin <brandonzchin@gmail.com>
2023-11-16 18:17:47 -0800

    Revised (Stable - Project Done)
    not edited (projects)

--- src/components/figure.js
+++ src/components/figure.js
@@ -1,11 +1,11 @@
 import React from "react";
-import image2 from "../adjustment/image2.jpeg";
+import image3 from "../adjustment/image3.jpg";
 import "./figure.css";
 
 const Figure = () => {
   return (
     <div className="figure-container">
-      <img id="image-2" src={image2} alt="" />
+      <img id="image-2" src={image3} alt="" />
     </div>
   );
 };
