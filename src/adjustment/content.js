f1daa943b957b586622163634d40150abe5ab1c5
Brandon Chin <brandonzchin@gmail.com>
2023-12-23 11:13:30 -0500

    update

--- src/adjustment/content.js
+++ src/adjustment/content.js
@@ -74,7 +74,7 @@ const projects = [
     title_in: "Travel Agency Internal Product Database",
     task: "Branch managers have raised an issue with the head office: New employees feel less motivated to sell current products due to the complexity of accessing inventory and collecting sales information.",
     process:
-      "I began by listening and pinpointing the company's issues during a meeting, then delved deeper by conducting 5 interviews with branch managers to gain a comprehensive grasp of the problem. Following brainstorming sessions, I suggested a website solution aimed at streamlining information flow. Once the proposal was approved, I gathered requirements and swiftly drafted a preliminary design to test user navigation within a day. Consulting with managers using the wireframe, I refined the user experience through collaborative adjustments. After some adjustments, I began building the website utilizing React and Node.js.",
+      "I began by listening and pinpointing the company's issues during a meeting, then delved deeper by conducting 5 interviews with branch managers to gain a comprehensive grasp of the problem. Following brainstorming sessions, I suggested a website solution aimed at streamlining information flow. Once the proposal was approved, I gathered requirements and swiftly drafted a preliminary design to test user navigation within a day. Consulting with managers using the wireframe, I refined the user experience through collaborative adjustments. After some adjustments, I began building the website utilizing React and Node.js. I utilized hooks like useState, useEffect, useRef, and several libraries to ensure the website's functionality. I then conducted a usability test with 2 branch managers to ensure the website's functionality and user experience.",
     solution:
       "A web page designed to streamline information presentation for agents, featuring an intuitive user interface and captivating graphics to efficiently communicate information. This aims to rapidly replace the conventional use of Excel sheets.",
   },
@@ -98,7 +98,7 @@ const projects = [
   {
     image: "./card-images/case-pm.png",
     title: "Amazon Case Study",
-    text: "I did my own case study on a hypothetical situation where Amazon executives opted to venture into the car retail industry. ",
+    text: "My own case study on a hypothetical situation where Amazon executives opted to venture into the car retail industry. ",
     tags: "Product Management",
     images: {
       image_1: "./card-images/case-pm.png",
@@ -131,77 +131,104 @@ const projects = [
   {
     image: "./card-images/popcorn.png",
     title: "Rate and Watchlist",
-    text: "To assess the likelihood of a person defaulting on their loan according to various factors",
+    text: "A web applicationm that allows users to rate and add movies to their watchlist",
     tags: "Passion Project:React.js:Node.js",
     images: {
       image_1: "./card-images/popcorn.png",
       image_2: "./card-images/popcorn-1.png",
       image_3: "./card-images/popcorn-2.png",
     },
-    title_in: "Loan Default Risk Assessment",
-    task: "I am given a comphrensive dataset with various factors (age, income, number of active loans, dependents, etc). Given the these factors, I am to determine the likelihood of defaulting on a loan. Additionally determine the best machine learning model to use and why?",
+    title_in: "Rate and Watchlist",
+    task: "While learning to build in react, I tasked with this project to build a web application that allows users to rate and add movies to their watchlist. I am to use the OMDB API to source the movies and display them on the web application. I am to use React.js to build the front-end and Node.js to build the back-end.",
     process:
-      "I first cleaned the data and fill the empty values/corrupted values using a predictive model (Data Reprocessing). I then chose specific variables for binning and decided the binning width according to various factors. Next, I constructed two ML models: (Naive Bayes and Nueral Net) I identified the one whom performed the best in comparision to the benchmark error rate. I then used the ensemble method(boosting and bagging) to assess if it would draw a lower error rate. Aftewards, I wrote my process and decision in a full-report.",
+      "Given the final image of the project, I started by breaking down each component that I could visualize, aiming for a modular structure that would ensure flexibility and scalability. I utilized useState and useEffect hooks to manage the state and side effects. While navigating the development phase, I encountered several obstacles that required restructuring of specific elements. With some aid from my online course, I was able to learn and build the application. ",
     solution:
-      "A 21-page documentation on my chose of ML algorithm to address the problem",
+      "A web-based platform enabling users to rate movies and include them in their watchlists, subsequently saving this information directly onto their devices.",
   },
   {
     image: "./card-images/pack.png",
     title: "Packing List",
-    text: "To assess the likelihood of a person defaulting on their loan according to various factors",
+    text: "A web applicationm that allows users to create and manage a packing list for their travels",
     tags: "Passion Project:React.js:Node.js",
     images: {
       image_1: "./card-images/pack.png",
       image_2: "./card-images/pack-1.png",
       image_3: "./card-images/pack-2.png",
     },
-    title_in: "Loan Default Risk Assessment",
-    task: "I am given a comphrensive dataset with various factors (age, income, number of active loans, dependents, etc). Given the these factors, I am to determine the likelihood of defaulting on a loan. Additionally determine the best machine learning model to use and why?",
+    title_in: "Packing List",
+    task: "While learning to build in react, I tasked with this project to build a web application that allows users to create and manage a packing list for their travels. I am to use React.js to build the front-end and Node.js to build the back-end.",
     process:
-      "I first cleaned the data and fill the empty values/corrupted values using a predictive model (Data Reprocessing). I then chose specific variables for binning and decided the binning width according to various factors. Next, I constructed two ML models: (Naive Bayes and Nueral Net) I identified the one whom performed the best in comparision to the benchmark error rate. I then used the ensemble method(boosting and bagging) to assess if it would draw a lower error rate. Aftewards, I wrote my process and decision in a full-report.",
+      "I started with identifying key actions that a user needs to perform to create a packing list. I then broke down each action into components and created a modular structure. I utilized useState hook to ensure items are properly rendered. ",
     solution:
-      "A 21-page documentation on my chose of ML algorithm to address the problem",
+      "A web-based platform enabling users to create and manage a packing list for their travels.",
   },
   {
     image: "./card-images/expense.png",
     title: "Simple Expense Tracker",
-    text: "To assess the likelihood of a person defaulting on their loan according to various factors",
+    text: "A web applicationm that allows users to track their expenses and debts",
     tags: "Passion Project:React.js:Node.js",
     images: {
       image_1: "./card-images/expense.png",
       image_2: "./card-images/expense-1.png",
       image_3: "./card-images/expense-2.png",
     },
-    title_in: "Loan Default Risk Assessment",
-    task: "I am given a comphrensive dataset with various factors (age, income, number of active loans, dependents, etc). Given the these factors, I am to determine the likelihood of defaulting on a loan. Additionally determine the best machine learning model to use and why?",
+    title_in: "Simple Expense Tracker",
+    task: "While learning to build in react, I tasked with this project to build a web application that allows users to track their expenses and debts. I am to use React.js to build the front-end and Node.js to build the back-end.",
     process:
-      "I first cleaned the data and fill the empty values/corrupted values using a predictive model (Data Reprocessing). I then chose specific variables for binning and decided the binning width according to various factors. Next, I constructed two ML models: (Naive Bayes and Nueral Net) I identified the one whom performed the best in comparision to the benchmark error rate. I then used the ensemble method(boosting and bagging) to assess if it would draw a lower error rate. Aftewards, I wrote my process and decision in a full-report.",
+      "I started with identifying key actions that a user needs to perform to track their expenses. I then broke down each action into components and created a modular structure. I utilized useState hook to ensure items are properly rendered. ",
     solution:
-      "A 21-page documentation on my chose of ML algorithm to address the problem",
+      "A web-based platform enabling users to track their expenses and debts.",
   },
   {
     image: "./card-images/pole.png",
-    title: "Polestar Growth Opportunity",
-    text: "To assess the likelihood of a person defaulting on their loan according to various factors",
-    tags: "Passion Project:React.js:Node.js",
+    title: "Challenges in Polestar's US EV Growth",
+    text: "A consulting project for Polestar, a Swedish electric car company, to identify growth opportunities in the US market",
+    tags: "Consulting:Market Research",
     images: {
       image_1: "./card-images/pole.png",
       image_2: "./card-images/pole-1.png",
       image_3: "./card-images/pole-2.png",
     },
-    title_in: "Loan Default Risk Assessment",
-    task: "I am given a comphrensive dataset with various factors (age, income, number of active loans, dependents, etc). Given the these factors, I am to determine the likelihood of defaulting on a loan. Additionally determine the best machine learning model to use and why?",
+    title_in: "Challenges in Polestar's US EV Growth",
+    task: "My team and I are tasked with identifying the challenges in Polestar's US EV growth and propose solutions to address these challenges. We are to conduct market research and analyze the data to identify the challenges and propose solutions.",
     process:
-      "I first cleaned the data and fill the empty values/corrupted values using a predictive model (Data Reprocessing). I then chose specific variables for binning and decided the binning width according to various factors. Next, I constructed two ML models: (Naive Bayes and Nueral Net) I identified the one whom performed the best in comparision to the benchmark error rate. I then used the ensemble method(boosting and bagging) to assess if it would draw a lower error rate. Aftewards, I wrote my process and decision in a full-report.",
+      "It began with market research and data analysis via the web and examination of their financial statements. Subsequently, we identified the challenges and proposed multiple solutions to address these issues. After narrowing our focus to two potential solutions, we presented our findings and broke down each solution into four components: Market Attractiveness, Competitive Advantage, NPV Analysis, and an Implementation Plan.",
     solution:
-      "A 21-page documentation on my chose of ML algorithm to address the problem",
+      "A powerpoint presentation on our findings and proposed solutions to address the challenges.",
   },
   {
-    image:
-      "https://images.unsplash.com/photo-1542903660-eedba2cda473?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    title: "Startup",
-    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
-    tags: "Instagram Facebook Tiktok",
+    image: "./card-images/startup.png",
+    title: "New Balance Marketing Campaign",
+    text: "A marketing project for New Balance increase brand awareness, organic search, and improve customer acquisition",
+    tags: "Marketing:Metaverse",
+    images: {
+      image_1: "./card-images/startup.png",
+      image_2: "./card-images/startup-1.png",
+      image_3: "./card-images/startup-2.png",
+    },
+    title_in: "New Balance: Metaverse Marketing Campaign",
+    task: "New Balance is looking to increase brand awareness, organic search, and improve customer acquisition. My team and I are tasked with creating a marketing campaign to address these issues. We are to conduct market research and analyze the data to identify the challenges and propose solutions.",
+    process:
+      "To pinpoint opportunities for enhancement, we employed NUVI, a social media analytics tool, to analyze their existing marketing presence. Following this, we pinpointed potential avenues for growth, namely the metaverse and leveraging digital influencers as marketing mediums. As a result, we crafted a marketing strategy centered around utilizing the metaverse and digital influencers to boost brand visibility, ultimately driving increased customer acquisition. To gauge the campaign's success, we outlined key metrics including Audience Growth Rate (%), Virality Coefficient (calculated as the Number of Shares divided by the Number of Impressions), and Click-Through Rate (CTR).",
+    solution:
+      "We assembled a comprehensive breakdown of our marketing campaign in a PowerPoint presentation, which we subsequently delivered to the Vice President of Marketing at New Balance. Furthermore, we meticulously crafted a 5-page paper outlining the intricacies of our strategy.",
+  },
+  {
+    image: "./card-images/startup.png",
+    title: "Student Startup: Sticker Pack",
+    text: "A custom sticker company that utilized student designs to create stickers",
+    tags: "Entrepreneurship:Marketing:E-commerce",
+    images: {
+      image_1: "./card-images/startup.png",
+      image_2: "./card-images/startup-1.png",
+      image_3: "./card-images/startup-2.png",
+    },
+    title_in: "Sticker Pack Startup",
+    task: "Identify market opportunities and create a product to address the market needs.",
+    process:
+      "As we explored opportunities within the college market, my team and I engaged in brainstorming sessions to conceptualize potential product ideas. Identifying students' strong inclination towards personalizing their school-provided laptops, we conducted a survey, revealing that 80% of students expressed interest in purchasing customizable stickers. Our value proposition centered around transforming any student's sketches into stickers, offering both convenience and affordability. Following our success in the rocket pitch, we secured funding for our venture and commenced sourcing manufacturers. I proceeded to create an e-commerce website using Shopify, while the team embarked on crafting designs. Subsequently, we initiated a marketing campaign aimed at promoting the website and our unique stickers.",
+    solution:
+      "We generated revenue within the first day of launch and broken even within 2 weeks.",
   },
 ];
 
